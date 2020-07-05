44172592cd2ed9f27ecdb8f6d7edcd26a2b65f48
Matan Dery <matandery1997@gmail.com>
2020-07-05 13:50:23 +0300

    added player/ai menu

--- tic-tac-ai.py
+++ tic-tac-ai.py
@@ -33,23 +33,23 @@ def next_player(grid):
     else:
         return 'X'
 
-def next_move(grid):
+def next_move(grid, symbol):
     move = input('Enter the coordinates:')
     try:
         move = move.split( )
         move = [int(i) for i in move]
-    except:
+    except ValueError:
         print('You should enter numbers!')
-        return next_move(grid)
+        return next_move(grid, symbol)
     else:
         if not (0 < move[0] < 4 and  0 < move[1] < 4):
             print('Coordinates should be from 1 to 3!')
-            return next_move(grid)
+            return next_move(grid, symbol)
         if grid[3-move[1]][move[0]-1] != '_':
             print('This cell is occupied! Choose another one!')
-            return next_move(grid)
+            return next_move(grid, symbol)
         else:
-            grid[3-move[1]][move[0]-1] = 'X'
+            grid[3-move[1]][move[0]-1] = symbol
             return True
 
 def ch_line(grid):
@@ -94,44 +94,76 @@ def check_win(grid):
         return ch_cross(grid)+' wins'
     return
 
-def easy_ai(grid):
+def easy_ai(grid, symbol):
     move = randint(0, 8)
     if grid[move//3][move % 3] == '_':
         print('Making move level "easy"')
-        grid[move // 3][move % 3] = 'O'
+        grid[move // 3][move % 3] = symbol
         return True
     else:
-        return easy_ai(grid)
+        return easy_ai(grid, symbol)
+
+def get_commands():
+    cmd = input('Input command: ')
+    if cmd == 'exit':
+        quit()
+    if cmd.split(' ')[0] == 'start':
+        cmd =cmd.split(' ')
+        if len(cmd) != 3:
+            print('Bad parameters!')
+        else:
+            players=[]
+            for i in cmd:
+                if i == 'easy':
+                    players.append('easy')
+                if i == 'user':
+                    players.append('user')
+            return players
+    else:
+        return get_commands()
 
 
-def main():
+def game(players):
     cells = '_________'
     grid = make_grid(cells)
     print_grid(grid)
-    player = 'human'
+
+    turn_cnt = 0
+
     while True:
         if ch_draw(grid) is not None:
             print('Draw')
-            break
+            main()
 
         won = check_win(grid)
         if won is not None:
             print(won)
-            break
+            main()
 
-        if player == 'pc':
-            moved = easy_ai(grid)
+        if turn_cnt % 2 == 0:
+            symbol = 'X'
+        else:
+            symbol = 'O'
+
+        if players[turn_cnt] == 'easy':
+            moved = easy_ai(grid, symbol)
             if moved:
                 print_grid(grid)
-                player = 'human'
+                turn_cnt = (turn_cnt + 1) % 2
                 continue
 
-        if player == 'human':
-            moved = next_move(grid)
+        if players[turn_cnt] == 'user':
+            moved = next_move(grid, symbol)
             if moved:
                 print_grid(grid)
-                player = 'pc'
+                turn_cnt = (turn_cnt + 1) % 2
                 continue
+
+
+def main():
+    players = get_commands()
+    game(players)
+
 
 
 if __name__ == '__main__':
