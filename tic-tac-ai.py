643146a287b17046692c9cfd7da09535bc3e74c9
Matan Dery <matandery1997@gmail.com>
2020-07-06 18:14:42 +0300

    Added Documentation

--- tic-tac-ai.py
+++ tic-tac-ai.py
@@ -1,14 +1,23 @@
 from random import randint, choice
-from collections import Counter
-from copy import copy, deepcopy
+from copy import deepcopy
 
-def make_grid(cells):
+
+def make_grid():
+    """
+    :return: 2d array for the game
+    """
+    cells = '_________'
     grid = []
     for i in range(0, len(cells), 3):
         grid.append(list(cells[i:i+3]))
     return grid
 
+
 def print_grid(grid):
+    """
+    :param grid: 2d array
+    prints current state of the game
+    """
     print('---------')
     for line in grid:
         print('| ', end='')
@@ -20,158 +29,158 @@ def print_grid(grid):
         print('|')
     print('---------')
 
-def next_player(grid):
-    cnt_x = 0
-    cnt_o = 1
-    for i in range(3):
-        for j in range(3):
-            if grid[i][j] == 'X':
-                cnt_x += 1
-            if grid[i][j] == 'O':
-                cnt_o += 1
-    if cnt_x == cnt_o:
-        return 'O'
-    else:
-        return 'X'
 
-def next_move(grid, symbol):
-    move = input('Enter the coordinates:')
+def user_move(grid, symbol):
+    """
+    :param grid: game bord
+    :param symbol: X or O
+    accepts input from user and let him play
+    """
+    move = input('Enter the coordinates:    ')
     try:
         move = move.split( )
         move = [int(i) for i in move]
     except ValueError:
         print('You should enter numbers!')
-        return next_move(grid, symbol)
+        return user_move(grid, symbol)
     else:
-        if not (0 < move[0] < 4 and  0 < move[1] < 4):
-            print('Coordinates should be from 1 to 3!')
-            return next_move(grid, symbol)
-        if grid[3-move[1]][move[0]-1] != '_':
+        if not (0 <= move[0] <= 2 and 0 <= move[1] <= 2):
+            print('Coordinates should be from 0 to 2!')
+            return user_move(grid, symbol)
+        if grid[move[0]][move[1]] != '_':
             print('This cell is occupied! Choose another one!')
-            return next_move(grid, symbol)
+            return user_move(grid, symbol)
         else:
-            grid[3-move[1]][move[0]-1] = symbol
-            return True
-
-def ch_line(grid):
-    for line in grid:
-        if line[0] == line[1] == line[2]:
-            if line[0] == 'X':
-                return 'X'
-            if line[0] == 'O':
-                return 'O'
-    return
-
-def ch_col(grid):
-    for i in range(3):
-        if grid[0][i] == grid[1][i] == grid[2][i]:
-            if grid[0][i] == 'X':
-                return 'X'
-            if grid[0][i] == 'O':
-                return 'O'
-    return
-
-def ch_cross(grid):
-    if (grid[0][0] == grid[1][1] == grid[2][2] or
-            grid[0][2] == grid[1][1] == grid[2][0]):
-        if grid[1][1] == 'X':
-            return 'X'
-        if grid[1][1] == 'O':
-            return 'O'
-    return
-
-def ch_draw(grid):
-    for i in grid:
-        if '_' in i:
+            grid[move[0]][move[1]] = symbol
             return
-    return 'Draw'
 
-def check_win(grid):
-    if ch_line(grid) is not None:
-        return ch_line(grid)+' wins'
-    if ch_col(grid) is not None:
-        return ch_col(grid)+' wins'
-    if ch_cross(grid) is not None:
-        return ch_cross(grid)+' wins'
-    return
+
+def win_chk(grid):
+    """
+    :param grid: bord state
+    :return: string of who won or None if nobdy won
+    """
+    def ch_line(grid):
+        for line in grid:
+            if line[0] == line[1] == line[2]:
+                if line[0] == 'X':
+                    return 'X'
+                if line[0] == 'O':
+                    return 'O'
+        return
+
+    def ch_col(grid):
+        for i in range(3):
+            if grid[0][i] == grid[1][i] == grid[2][i]:
+                if grid[0][i] == 'X':
+                    return 'X'
+                if grid[0][i] == 'O':
+                    return 'O'
+        return
+
+    def ch_cross(grid):
+        if (grid[0][0] == grid[1][1] == grid[2][2] or
+                grid[0][2] == grid[1][1] == grid[2][0]):
+            if grid[1][1] == 'X':
+                return 'X'
+            if grid[1][1] == 'O':
+                return 'O'
+        return
+
+    def check_win(grid):
+        if ch_line(grid) is not None:
+            return ch_line(grid)+' wins'
+        if ch_col(grid) is not None:
+            return ch_col(grid)+' wins'
+        if ch_cross(grid) is not None:
+            return ch_cross(grid)+' wins'
+        return
+
+    return check_win(grid)
+
 
 def easy_ai(grid, symbol):
-    move = randint(0, 8)
-    if grid[move//3][move % 3] == '_':
-        print('Making move level "easy"')
-        grid[move // 3][move % 3] = symbol
-        return True
-    else:
-        return easy_ai(grid, symbol)
+    """
+    :param grid: game bord
+    :param symbol: X or O
+    """
+    move = choice(find_avil(grid))
+    print('Making move level "easy"')
+    grid[move[0]][move[1]] = symbol
 
-def cross_can_win(grid, symbol):
-    if (grid[0][0] + grid[1][1] + grid[2][2]).count(symbol) == 2:
+
+def medium_ai(grid, symbol):
+    """
+    :param grid: game bord
+    :param symbol: X or O
+    """
+    def cross_can_win(grid, symbol):
+        if (grid[0][0] + grid[1][1] + grid[2][2]).count(symbol) == 2:
+            for i in range(3):
+                if grid[i][i] == '_':
+                    return (i, i)
+        if (grid[0][2] + grid[1][1] + grid[2][0]).count(symbol) == 2:
+            for i in range(3):
+                if grid[i][2 - i] == '_':
+                    return (i, 2 - i)
+        return False
+
+    def rows_can_win(grid, symbol):
+        for i, line in enumerate(grid):
+            if line.count(symbol) == 2:
+                for j, char in enumerate(line):
+                    if char == '_':
+                        return (i, j)
+        return False
+
+    def col_can_win(grid, symbol):
         for i in range(3):
-            if grid[i][i] == '_':
-                return (i, i)
-    if(grid[0][2] + grid[1][1] + grid[2][0]).count(symbol) == 2:
-        for i in range(3):
-            if grid[i][2-i] == '_':
-                return (i, 2-i)
-    return False
+            if (grid[0][i] + grid[1][i] + grid[2][i]).count(symbol) == 2:
+                for j in range(3):
+                    if grid[j][i] == '_':
+                        return (j, i)
+        return False
 
+    def can_med_win(grid, symbol):
+        """
+        chacks if symbol can win next turn
+        """
+        move = cross_can_win(grid, symbol)
+        if move != False:
+            return move
+        move = rows_can_win(grid, symbol)
+        if move != False:
+            return move
+        move = col_can_win(grid, symbol)
+        if move != False:
+            return move
+        return False
 
-
-def rows_can_win(grid, symbol):
-    for i, line in enumerate(grid):
-        if line.count(symbol) == 2:
-            for j, char in enumerate(line):
-                if char == '_':
-                    return (i, j)
-    return False
-
-def col_can_win(grid, symbol):
-    for i in range(3):
-        if (grid[0][i] + grid[1][i] + grid[2][i]).count(symbol) == 2:
-            for j in range(3):
-                if grid[j][i] == '_':
-                    return (j, i)
-    return False
-
-def can_med_win(grid, symbol):
-    move = cross_can_win(grid, symbol)
-    if move != False:
-        return move
-    move = rows_can_win(grid, symbol)
-    if move != False:
-        return move
-    move = col_can_win(grid, symbol)
-    if move != False:
-        return move
-    return False
-
-
-
-
-
-def med_ai(grid, symbol):
-    can_win = can_med_win(grid, symbol)
-    if can_win != False:
-        grid[can_win[0]][can_win[1]] = symbol
-        return True
-    else:
-        if symbol == 'X':
-            adv_sym = 'O'
-        elif symbol == 'O':
-            adv_sym = 'X'
-        can_win = can_med_win(grid, adv_sym)
+    def med_ai(grid, symbol):
+        can_win = can_med_win(grid, symbol)
         if can_win != False:
             grid[can_win[0]][can_win[1]] = symbol
-            return True
-    move = randint(0, 8)
-    if grid[move//3][move % 3] == '_':
+            return
+        else:
+            switch_sym = {'X': 'O', 'O': 'X'}
+            adv_sym = switch_sym[symbol]
+            can_win = can_med_win(grid, adv_sym)
+            if can_win != False:
+                grid[can_win[0]][can_win[1]] = symbol
+                return
+        move = choice(find_avil(grid))
         print('Making move level "medium"')
-        grid[move // 3][move % 3] = symbol
-        return True
-    else:
-        return med_ai(grid, symbol)
+        grid[move[0]][move[1]] = symbol
+        return
+
+    med_ai(grid, symbol)
+
 
 def find_avil(grid):
+    """
+    :param grid:  game bord
+    :return: list of empty spots
+    """
     available_spots =[]
     for i in range(3):
         for j in range(3):
@@ -179,95 +188,87 @@ def find_avil(grid):
                 available_spots.append((i, j))
     return available_spots
 
-def minimax_init(grid, depth, maxim, sym):
-    if len(find_avil(grid)) == 9:
-        best_spot = choice([(0, 0), (2, 2), (2, 0), (0, 2)])
+
+def hard_ai(grid, depth, sym):
+    """
+    :param grid:
+    :param depth:
+    :param sym:
+    hard AI implementation using minimax
+    """
+    def minimax_init(grid, depth, sym):
+        """
+        initialising conditions for minimax
+        """
+        if len(find_avil(grid)) == 9:
+            best_spot = choice([(0, 0), (2, 2), (2, 0), (0, 2)])
+            grid[best_spot[0]][best_spot[1]] = sym
+            return
+        best_score = -100
+        fake_grid = deepcopy(grid)
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == '_':
+                    fake_grid[i][j] = sym
+                    score = minimax(fake_grid, depth, False, sym)
+                    fake_grid[i][j] = '_'
+                    #print(score , (i, j))
+                    if score > best_score:
+                        best_score = score
+                        best_spot = (i, j)
+
         grid[best_spot[0]][best_spot[1]] = sym
-        return
-    switch_sym = {'X':'O', 'O':'X'}
-    bestScore = -10000000
-    for i in range(3):
-        for j in range(3):
-            if grid[i][j] == '_':
-                fake_grid = deepcopy(grid)
-                fake_grid[i][j] = sym
-                score = minimax(fake_grid, depth, False, sym)
-                fake_grid[i][j] = '_'
-                print(score , (i, j))
-                if (score > bestScore):
-                    bestScore = score
-                    best_spot = (i, j)
-
-    grid[best_spot[0]][best_spot[1]] = sym
 
 
-def minimax(grid, depth, maxim, sym):
-    switch_sym = {'X':'O', 'O':'X'}
-    adv_sym = switch_sym[sym]
-
-    fake_grid = grid
-    available_spots = find_avil(fake_grid)
-    won = check_win(fake_grid)
-    if won is not None:
-        if sym in won:
-            return 100
-        elif adv_sym in won:
-            return -100
-        else:
+    def minimax(grid, depth, maxim, sym):
+        """
+        :param grid: game bord state after initialiser move
+        :param depth: depth of search - not used
+        :param maxim: Is maximising player
+        :param sym: symbol of maximising player
+        :return: return max score possible given minimax
+        """
+        switch_sym = {'X': 'O', 'O': 'X'}
+        adv_sym = switch_sym[sym]
+        available_spots = find_avil(grid)
+        won = win_chk(grid)
+        if won is not None:
+            if sym in won:
+                return 10
+            elif adv_sym in won:
+                return -10
+            else:
+                return 0
+        elif len(available_spots) == 0:
             return 0
-    elif len(available_spots) == 0:
-        return 0
-    else:
-        scores = []
-        if maxim:
-            available_spots = find_avil(fake_grid)
-            for spot in available_spots:
-                fake_grid[spot[0]][spot[1]] = sym
-                evalu = minimax(fake_grid, depth-1, False, sym)
-                fake_grid[spot[0]][spot[1]] = '_'
-                scores.append(evalu)
-            return max(scores)
         else:
-            available_spots = find_avil(fake_grid)
-            for spot in available_spots:
-                fake_grid[spot[0]][spot[1]] = adv_sym
-                evalu = minimax(fake_grid, depth-1, True, sym)
-                fake_grid[spot[0]][spot[1]] = '_'
-                scores.append(evalu)
-            return min(scores)
+            scores = []
+            if maxim:
+                available_spots = find_avil(grid)
+                for spot in available_spots:
+                    grid[spot[0]][spot[1]] = sym
+                    evalu = minimax(grid, depth-1, False, sym)
+                    grid[spot[0]][spot[1]] = '_'
+                    scores.append(evalu)
+                return max(scores)
+            else:
+                available_spots = find_avil(grid)
+                for spot in available_spots:
+                    grid[spot[0]][spot[1]] = adv_sym
+                    evalu = minimax(grid, depth-1, True, sym)
+                    grid[spot[0]][spot[1]] = '_'
+                    scores.append(evalu)
+                return min(scores)
 
-    # fake_grid = grid.copy()
-    # if symbol == 'X':
-    #     adv_sym = 'O'
-    # else:
-    #     adv_sym = 'X'
-    # available_spots = find_avil(fake_grid)
-    # scores = [0]*len(available_spots)
-    # won = check_win(fake_grid)
-    # if won is not None:
-    #     if symbol in won:
-    #         if cnt % 2 == 0:
-    #             return 1
-    #         return -1
-    #     else:
-    #         if cnt % 2 == 0:
-    #             return -1
-    #         return 1
-    #
-    # if len(available_spots) != 0:
-    #     next_turn = available_spots[0]
-    #     fake_grid[next_turn[0]][next_turn[1]] = symbol
-    #
-    #     return minimax(fake_grid, adv_sym, cnt+1)
-    # else:
-    #     return 0
-
-
-
-
+    minimax_init(grid, depth, sym)
 
 
 def get_commands():
+    """
+    starts a new game or quit
+    :return list of players
+    """
+    print('Format: start p1 p2  or exit \ngame modes: user / easy / medium / hard')
     cmd = input('Input command: ')
     if cmd == 'exit':
         quit()
@@ -278,37 +279,59 @@ def get_commands():
         else:
             players=[]
             for i in cmd:
-                if i == 'easy':
-                    players.append('easy')
-                if i == 'user':
-                    players.append('user')
-                if i == "medium":
-                    players.append('medium')
-                if i == "hard":
-                    players.append('hard')
-
-
+                if i == 'easy' or i == 'user' or i == "medium" or i == "hard":
+                    players.append(i)
+            if len(players) != 2:
+                print('Bad parameters!\n')
+                return get_commands()
             return players
     else:
         return get_commands()
 
 
+def pick_player(player, grid, symbol):
+    """
+    :param player: players turn
+    :param grid: game bord
+    :param symbol: X or O
+    """
+    if player == 'easy':
+        easy_ai(grid, symbol)
+        print_grid(grid)
+        return
+
+    if player == 'medium':
+        medium_ai(grid, symbol)
+        print_grid(grid)
+        return
+
+    if player == 'hard':
+        hard_ai(grid, 9, symbol)
+        print_grid(grid)
+        return
+
+    if player == 'user':
+        user_move(grid, symbol)
+        print_grid(grid)
+        return
+
+
 def game(players):
-    cells = '_________'
-    grid = make_grid(cells)
+    """
+    manages the game
+    :param players: a list of players
+    """
+    grid = make_grid()
     print_grid(grid)
-
     turn_cnt = 0
-
     while True:
-        print(players)
-        if ch_draw(grid) is not None:
-            print('Draw')
-            main()
-
-        won = check_win(grid)
+        won = win_chk(grid)
         if won is not None:
             print(won)
+            main()
+
+        if len(find_avil(grid)) == 0:
+            print('Draw')
             main()
 
         if turn_cnt % 2 == 0:
@@ -316,34 +339,8 @@ def game(players):
         else:
             symbol = 'O'
 
-        print(players[turn_cnt])
-        if players[turn_cnt] == 'easy':
-            moved = easy_ai(grid, symbol)
-            if moved:
-                print_grid(grid)
-                turn_cnt = (turn_cnt + 1) % 2
-                continue
-
-        if players[turn_cnt] == 'medium':
-            moved = med_ai(grid, symbol)
-            if moved:
-                print_grid(grid)
-                turn_cnt = (turn_cnt + 1) % 2
-                continue
-
-        if players[turn_cnt] == 'hard':
-            minimax_init(grid, 1000000000, True, symbol)
-            print_grid(grid)
-            turn_cnt = (turn_cnt + 1) % 2
-            continue
-
-
-        if players[turn_cnt] == 'user':
-            moved = next_move(grid, symbol)
-            if moved:
-                print_grid(grid)
-                turn_cnt = (turn_cnt + 1) % 2
-                continue
+        pick_player(players[turn_cnt], grid, symbol)
+        turn_cnt = (turn_cnt + 1) % 2
 
 
 def main():
